6212f968f11b7bb40f46b355dbe8732778a634bf
Wladek <vladmasliuk@gmail.com>
2021-11-08 11:16:11 +0100

    connect do mongo db

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=> console.log('DB is connected'))
+.catch((err)=> console.log(err));
 
 app.listen(8800, ()=>{
     console.log('Server is runing');

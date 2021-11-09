6f3daddcec7a4c32731f002a5cda48ba9e0530e2
Wladek <vladmasliuk@gmail.com>
2021-11-09 17:56:45 +0100

    add project to portfolio

---
+++ routes/project.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const Project = require('../models/Project');
+const verify = require("../verifyToken");
+
+// add
+router.post("/", verify, async (req, res) => {
+    if(req.user.isAdmin){
+        const newProject = new Project(req.body);
+
+        try{
+            const savedProject = await newProject.save();
+            res.status(201).json(savedProject);
+        }catch(err){
+            res.status(500).json(err);
+        }
+    } else{
+        res.status(403).json("You are not admin")
+    }
+})
+
+
+module.exports = router;
